2d7f1c901e30703f416731daa2eef0da279bb143
Michael Selvesakis <miselve@cs.ihu.gr>
2023-10-23 23:09:12 +0300

    Broken Heart

--- components/guide.tsx
+++ components/guide.tsx
@@ -1,14 +1,11 @@
 import styles from '../styles/guide.module.css';
 import showdown from "showdown";
 import parse from 'html-react-parser';
+import {readFileSync} from "fs";
 
 export default function Guide() {
     var converter = new showdown.Converter();
-    var md = '[**Showdown**](http://www.showdownjs.com) is *great*\n' +
-         'because:\n\n' +
-         ' - it\'s easy to use\n' +
-         ' - it\'s extensible\n' +
-         ' - works in the server and in the browser';
+    var md = fs.readFileSync(process.cwd() + '/MDGuides/test.md', {encoding: 'utf-8',});
     let html = converter.makeHtml(md);
 
 
