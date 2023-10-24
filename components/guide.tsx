51d3f54ea4083735e7c35376473aec01be0fa7de
Michael Selvesakis <miselve@cs.ihu.gr>
2023-10-24 09:52:00 +0300

    unfuck guide

--- components/guide.tsx
+++ components/guide.tsx
@@ -4,28 +4,13 @@ import parse from 'html-react-parser';
 import { useEffect, useState } from 'react';
 
 export default function Guide() {
-    const [content, setContent] = useState<JSX.Element[] | null>(null); // Explicitly define the type
 
-    useEffect(() => {
-        const fetchMarkdown = async () => {
-            try {
-                const response = await fetch('/MDGuides/test.md');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                const md = await response.text();
-                const converter = new showdown.Converter();
-                const html = converter.makeHtml(md);
-                const out:JSX.Element = parse(html) ; // Explicitly typecast to Element
-                setContent(out)
-            } catch (error) {
-                console.error('Error fetching or parsing Markdown:', error);
-            }
-        };
-
-        fetchMarkdown();
-    }, []);
-
+    const md =`### Η συντακτική δομή της SELECT είναι η εξής:
+         SELECT column1, column2, ...
+         FROM table_name`;
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(md);
+    const out = parse(html)
     return (
         <div className={styles.container}>
             {out}
