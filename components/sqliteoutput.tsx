d15ea9ee38926df5a7292a5571b9abca18a73b58
BillisC <26091373+BillisC@users.noreply.github.com>
2023-10-24 23:31:16 +0300

    fix key error

--- components/sqliteoutput.tsx
+++ components/sqliteoutput.tsx
@@ -1,12 +1,12 @@
 
 import styles from '../styles/sqliteoutput.module.css';
 import { useSQLiteData } from './sqlite/sqlite';
-
-export default function SQLiteOutput() {
+var query = ''
+export default function SQLiteOutput(query) {
     const {rows} = useSQLiteData("select * from t");
     return (
         <div className={styles.container}>
-            <table><tbody>{rows.map((row) => <tr>{row.map((col) => <td>{col}</td>)}</tr>)}</tbody>
+            <table><tbody>{rows.map((row, index) => <tr key={index}>{row.map((col,index) => <td key = {index}>{col}</td>)}</tr>)}</tbody>
             </table>
         </div>
     )
