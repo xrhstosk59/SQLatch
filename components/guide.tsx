18cb570affe36c349bf0bde636c96e3761e616a5
Michael Selvesakis <miselve@cs.ihu.gr>
2023-10-26 19:32:08 +0300

    Added Next/Previous Button to Guide Panel

--- components/guide.tsx
+++ components/guide.tsx
@@ -1,22 +1,48 @@
 import styles from '../styles/guide.module.css';
 import showdown from "showdown";
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef  } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 
+
+
 export default function Guide() {
     const [md, setMd] = useState('');
+    const Guides = ['test.md', 'test1.md'];
+    const idxRef = useRef(0);
 
+    const fetchMd = async () => {
+        try {
+            const response = await fetch('/MDGuides/' + Guides[idxRef.current]);
+            const text = await response.text();
+            setMd(text);
+            
+            
+        } catch (error) {
+            console.error('Error fetching the file: ', error);
+
+        }
+        
+    };
+
+    const loadNextGuide = () => {
+        if (idxRef.current < Guides.length - 1) {
+            idxRef.current++;
+            console.log(idxRef.current);
+            fetchMd();
+        }
+    };
+
+    const loadPrevGuide = () => {
+        if (idxRef.current > 0) {
+            idxRef.current--;
+            console.log(idxRef.current);
+            fetchMd();
+        }
+    };
+
+    
     useEffect(() => {
-        const fetchMd = async () => {
-            try {
-                const response = await fetch('/MDGuides/test.md');
-                const text = await response.text();
-                setMd(text);
-            } catch (error) {
-                console.error('Error fetching the file: ', error);
-            }
-        };
         fetchMd();
     }, []);
 
@@ -28,11 +54,13 @@ export default function Guide() {
             <Accordion defaultActiveKey="0"><Accordion.Item eventKey="0">
                 <Accordion.Header>Guide</Accordion.Header>
                 <Accordion.Body className="d-flex justify-content-between">
-                <Button variant="secondary">Previous</Button>
-                <Button variant="success">Next</Button></Accordion.Body>
+                    <Button variant="secondary" onClick={loadPrevGuide}>Previous</Button>
+                    <Button variant="success" onClick={loadNextGuide}>Next</Button>
+                </Accordion.Body>
                 <Accordion.Body dangerouslySetInnerHTML={{ __html: html }}></Accordion.Body>
-            
+
             </Accordion.Item>
             </Accordion> </div>
     );
+
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
     return (
         <>
             <Head>
+            <link rel="icon" type="image/png" sizes="180x180" href="/favicon.png"></link>
                 <title>SQLatch - POC</title>
             </Head>
             <div className={styles.topcontainer}>
