e16a2daecea528ee76e9921a9c35508d9a953d05
BillisC <26091373+BillisC@users.noreply.github.com>
2023-11-14 23:43:57 +0200

    change guide button visibility

--- components/guide.tsx
+++ components/guide.tsx
@@ -29,6 +29,14 @@ export default function Guide() {
     const [ MDTasks, setMDTasks ] = useState();
     const [ MDScenarios, setMDScenarios ] = useState();
 
+    const isPrevShown = (type: number) => {
+        return idxState[type] > 0;
+    };
+
+    const isNextShown = (type: number) => {
+        return idxState[type] < LTS[type].length - 1;
+    };
+
     const loadNextGuide = (type: number) => {
         if (idxState[type] < LTS[type].length - 1) {
             setIdxState(prevState => {
@@ -77,28 +85,34 @@ export default function Guide() {
         <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }} className={styles.container}>
             <Accordion defaultActiveKey="0"><Accordion.Item eventKey="0">
                 <Accordion.Header>Μαθήματα</Accordion.Header>
+                {isPrevShown(0) || isNextShown(0) ? (
                 <Accordion.Body className="d-flex justify-content-between">
-                    <Button variant="secondary" onClick={() => { loadPrevGuide(0) }}>Προηγούμενο</Button>
-                    <Button variant="success" onClick={() => { loadNextGuide(0) }}>Επόμενο</Button>
+                    <Button variant="secondary" onClick={() => { loadPrevGuide(0) }} disabled={!isPrevShown(0)}>Προηγούμενο</Button>
+                    <Button variant="success" onClick={() => { loadNextGuide(0) }} disabled={!isNextShown(0)}>Επόμενο</Button>
                 </Accordion.Body>
+                ) : <></>}
                 <Accordion.Body dangerouslySetInnerHTML={{ __html: MDLessons }}></Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="1">
                 <Accordion.Header>Ασκήσεις</Accordion.Header>
+                {isPrevShown(1) || isNextShown(1) ? (
                 <Accordion.Body className="d-flex justify-content-between">
-                    <Button variant="secondary" onClick={() => { loadPrevGuide(1) }}>Προηγούμενο</Button>
-                    <Button variant="success" onClick={() => { loadNextGuide(1) }}>Επόμενο</Button>
+                    <Button variant="secondary" onClick={() => { loadPrevGuide(1) }} disabled={!isPrevShown(1)}>Προηγούμενο</Button>
+                    <Button variant="success" onClick={() => { loadNextGuide(1) }} disabled={!isNextShown(1)}>Επόμενο</Button>
                 </Accordion.Body>
+                ) : <></>}
                 <Accordion.Body dangerouslySetInnerHTML={{ __html: MDTasks }}></Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="2">
                 <Accordion.Header>Σενάρια</Accordion.Header>
+                {isPrevShown(2) || isNextShown(2) ? (
                 <Accordion.Body className="d-flex justify-content-between">
-                    <Button variant="secondary" onClick={() => { loadPrevGuide(2) }}>Προηγούμενο</Button>
-                    <Button variant="success" onClick={() => { loadNextGuide(2) }}>Επόμενο</Button>
+                    <Button variant="secondary" onClick={() => { loadPrevGuide(2) }} disabled={!isPrevShown(2)}>Προηγούμενο</Button>
+                    <Button variant="success" onClick={() => { loadNextGuide(2) }} disabled={!isNextShown(2)}>Επόμενο</Button>
                 </Accordion.Body>
+                ) : <></>}
                 <Accordion.Body dangerouslySetInnerHTML={{ __html: MDScenarios }}></Accordion.Body>
             </Accordion.Item>
             </Accordion> 
